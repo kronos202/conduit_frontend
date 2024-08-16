51c8cd1e303780b2f7a226f0188d71cae2bdefbf
kronos202 <kronosss2002@gmail.com>
2024-08-16 16:09:13 +0700

    change .env

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,8 @@
 const config = {
-  baseUrl: "https://backendconduit.up.railway.app/api/",
+  baseUrl:
+    `${import.meta.env.VITE_ENVIRONMENT as string}` === "development"
+      ? `${import.meta.env.VITE_DEV_BACKEND_URL as string}`
+      : `${import.meta.env.VITE_PRODUCTION_BACKEND_URL as string}`,
 };
 
 export default config;
