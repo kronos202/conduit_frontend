1e5d1375b006e04b9201ce154a38ea2d9616aa79
kronos202 <kronosss2002@gmail.com>
2024-08-19 01:38:34 +0700

    .

--- src/schemaValidations/auth.schema.ts
+++ src/schemaValidations/auth.schema.ts
@@ -16,10 +16,14 @@ export const LoginGoogleBody = z
 
 export type LoginGoogleBodyType = z.TypeOf<typeof LoginGoogleBody>;
 
-export const RegisterBody = LoginBody.extend({
-  email: z.string().email().min(8),
-  password: z.string().min(8),
-}).strict();
+export const RegisterBody = z
+  .object({
+    email: z.string().email().min(8),
+    password: z.string().min(8),
+    username: z.string().min(8),
+    avatar: z.string().optional(),
+  })
+  .strict();
 
 export type RegisterBodyType = z.TypeOf<typeof RegisterBody>;
 
