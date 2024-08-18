5c6ec4ebc2db055532c732204958075dc8056afe
kronos202 <kronosss2002@gmail.com>
2024-08-19 01:16:29 +0700

    .

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,8 +1,8 @@
 const config = {
   baseUrl:
-    `${import.meta.env.VITE_ENVIRONMENT as string}` === "development"
-      ? `${import.meta.env.VITE_DEV_BACKEND_URL as string}`
-      : `${import.meta.env.VITE_PRODUCTION_BACKEND_URL as string}`,
+    `${import.meta.env.VITE_ENVIRONMENT}` === "development"
+      ? `${import.meta.env.VITE_DEV_BACKEND_URL}`
+      : `${import.meta.env.VITE_PRODUCTION_BACKEND_URL}`,
 };
 
 export default config;
